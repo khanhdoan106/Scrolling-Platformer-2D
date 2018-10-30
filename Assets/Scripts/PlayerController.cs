8238d86a1191ff2277cd5d6e1c8d6cf681ee4085
Doan_Khanh <doank106@hsd.k12.or.us>
2018-10-30 14:15:02 -0700

    adding playerController script for simple movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    private RigidBody m_rb;
+	// Use this for initialization
+	void Start () {
+        m_rb = GetComponent<RigidBody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float movement = Input.GetAxis("Horizontal");
+        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F))
+	}
+}
